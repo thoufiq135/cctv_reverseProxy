58953e4dc1dbe576a39fba6b83569baa3b4cf85d
Thoufiq <147231670+thoufiq135@users.noreply.github.com>
2026-07-03 13:26:14 +0530

    Refactor proxy middleware and add CORS headers
    Refactor proxy middleware setup for cleaner code and add CORS headers.

--- stream.js
+++ stream.js
@@ -1,29 +1,26 @@
 const express = require("express");
-
-const {
-    createProxyMiddleware
-} = require("http-proxy-middleware");
-
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const router = express.Router();
 
-
-
 router.use(
-    "/",
-    createProxyMiddleware({
+  createProxyMiddleware({
+    target: "http://100.117.158.50:8888",
+    changeOrigin: true,
+    ws: true,
 
-        target: "http://100.117.158.50:8888",
-
-        changeOrigin: true,
-
-        ws: true,
-
-        pathRewrite: {
-            "^/": "/"
-        }
-
-    })
+    onProxyRes: (proxyRes, req, res) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, ngrok-skip-browser-warning"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, OPTIONS"
+      );
+    },
+  })
 );
 
 module.exports = router;
